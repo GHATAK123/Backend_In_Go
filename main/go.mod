ee911c84da44ecf7700afb4c209bbb77fd57356d
PAN142 <prakash.anand@maersk.com>
2022-06-17 17:05:31 +0530

    initial commit 1.o

---
+++ main/go.mod
@@ -0,0 +1,5 @@
+module ghatu.backend/main
+
+go 1.17
+
+require github.com/rccsdevops/go v0.0.0-20210713174927-609e207099cf // indirect
---
+++ main/welcome.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rccsdevops/go/repo"
+)
+
+func main() {
+
+	fmt.Println("Learn Backend using Go!")
+	fmt.Println(repo.SayHi())
+
+}
